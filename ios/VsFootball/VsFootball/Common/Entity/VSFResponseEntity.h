67fbb9b67e749246728ce1c6eb4022ebd4e8616d
Jeremy <Jeremy@Jeremys-Mac-mini.local>
2013-07-23 22:37:10 +0800

    Jeremy: add ResponseEntity

---
+++ ios/VsFootball/VsFootball/Common/Entity/VSFResponseEntity.h
@@ -0,0 +1,13 @@
+//
+//  VSFResponseEntity.h
+//  VsFootball
+//
+//  Created by Jeremy on 13-7-23.
+//  Copyright (c) 2013年 engagemobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VSFResponseEntity : NSObject
+
+@end
