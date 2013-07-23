767fd207234212f60593df0da070b66e66a12973
Jeremy Lu <zhenzhelu@hengtiansoft.com>
2013-07-23 22:55:43 +0800

    Add a utils package

---
+++ android/src/com/engagemobile/vsfootball/utils/ShaUtil.java
@@ -0,0 +1,21 @@
+package com.engagemobile.vsfootball.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class ShaUtil {
+	public static String getSHA(String val) throws NoSuchAlgorithmException {
+		MessageDigest sha = MessageDigest.getInstance("SHA-1");
+		sha.update(val.getBytes());
+		byte[] m = sha.digest();
+		return getString(m);
+	}
+
+	private static String getString(byte[] b) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < b.length; i++) {
+			sb.append(b[i]);
+		}
+		return sb.toString();
+	}
+}
