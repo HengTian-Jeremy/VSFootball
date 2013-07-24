f0d1f75b51257f689656b44aab20feacf5e1d9a8
Jeremy Lu <zhenzhelu@hengtiansoft.com>
2013-07-24 17:36:45 +0800

    Add Flurry

--- android/src/com/engagemobile/vsfootball/activity/VsFootballActivity.java
+++ android/src/com/engagemobile/vsfootball/activity/VsFootballActivity.java
@@ -1,11 +1,11 @@
 package com.engagemobile.vsfootball.activity;
 
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
 import com.engagemobile.vsfootball.R;
+import com.flurry.android.FlurryAgent;
 
 public class VsFootballActivity extends Activity {
 
@@ -16,10 +16,25 @@ public class VsFootballActivity extends Activity {
 	}
 
 	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		FlurryAgent.setContinueSessionMillis(1000);
+		FlurryAgent.onStartSession(this, "J87QSPCGD484RV66QSF4");
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		FlurryAgent.onEndSession(this);
+		super.onStop();
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.vs_football, menu);
 		return true;
 	}
-	
+
 }
