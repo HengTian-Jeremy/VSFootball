f15c1a6158bffa6c06551008d9c783e81ee64cac
Jeremy Lu <zhenzhelu@hengtiansoft.com>
2013-07-25 19:22:52 +0800

    Design MainActivity,implement Navigation Drawer and SlidingDrawer.

--- android/src/com/engagemobile/vsfootball/activity/LoginActivity.java
+++ android/src/com/engagemobile/vsfootball/activity/LoginActivity.java
@@ -73,25 +73,28 @@ public class LoginActivity extends VsFootballActivity {
 		mChkRemember.setChecked(mIsRememberPassword);
 
 		mChkRemember.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
+
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView,
 					boolean isChecked) {
 				mIsRememberPassword = isChecked;
 			}
-			
+
 		});
 		mBtnLogin.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				if (mInputManager.isActive())
-					mInputManager.hideSoftInputFromWindow(mBtnLogin.getWindowToken(), 0);
+					mInputManager.hideSoftInputFromWindow(
+							mBtnLogin.getWindowToken(), 0);
 				if (validateInput()) {
 					handleLogin();
+					startActivity(new Intent(mContext, MainActivity.class));
 				}
 				if (mIsRememberPassword) {
-					userInfo.edit().putBoolean("isRemember", mIsRememberPassword)
+					userInfo.edit()
+							.putBoolean("isRemember", mIsRememberPassword)
 							.commit();
 					userInfo.edit()
 							.putString("username",
@@ -113,8 +116,7 @@ public class LoginActivity extends VsFootballActivity {
 			}
 		});
 	}
-	
-	
+
 	/**
 	 * Handle the login request
 	 */
@@ -134,11 +136,15 @@ public class LoginActivity extends VsFootballActivity {
 			@Override
 			protected Response doInBackground(String... params) {
 				// TODO using Resteasy framework instead
-				/*String url = "http://vsf001.engagemobile.com/login?username=zxj&password=123";
-				UserService_New service = ProxyFactory.create(UserService_New.class, url);
-				Response response = service.userLogin("", "");
-				
-				Log.d(TAG, response.getEntity().toString());*/
+				/*
+				 * String url =
+				 * "http://vsf001.engagemobile.com/login?username=zxj&password=123"
+				 * ; UserService_New service =
+				 * ProxyFactory.create(UserService_New.class, url); Response
+				 * response = service.userLogin("", "");
+				 * 
+				 * Log.d(TAG, response.getEntity().toString());
+				 */
 				UserService service = new UserService();
 				String username = mEtUsername.getText().toString();
 				String password = "";
@@ -153,11 +159,14 @@ public class LoginActivity extends VsFootballActivity {
 			@Override
 			protected void onPostExecute(Response result) {
 				mProgress.dismiss();
-				if (result != null && result.getStatusCode() == HttpStatus.SC_OK) {
+				if (result != null
+						&& result.getStatusCode() == HttpStatus.SC_OK) {
 					// TODO
-					Toast.makeText(LoginActivity.this, result.getContent(), Toast.LENGTH_SHORT).show();
+					Toast.makeText(LoginActivity.this, result.getContent(),
+							Toast.LENGTH_SHORT).show();
 				} else {
-					Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+					Toast.makeText(LoginActivity.this, "Login Failed!",
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 
---
+++ android/src/com/engagemobile/vsfootball/view/adapter/PlayAdapter.java
@@ -0,0 +1,83 @@
+package com.engagemobile.vsfootball.view.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.engagemobile.vsfootball.R;
+import com.engagemobile.vsfootball.bean.Play;
+
+public class PlayAdapter extends BaseAdapter {
+	private List<Play> mPlayList;
+	private Context mContext;
+
+	public PlayAdapter(Context mContext, List<Play> mPlayList) {
+		super();
+		this.mPlayList = mPlayList;
+		this.mContext = mContext;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return mPlayList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return mPlayList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		ViewHolder mViewHolder = null;
+		if (convertView != null)
+			mViewHolder = (ViewHolder) convertView.getTag();
+		else {
+			mViewHolder = new ViewHolder();
+			convertView = LayoutInflater.from(mContext).inflate(
+					R.layout.play_list_item, null);
+			mViewHolder.mTextView = (TextView) convertView
+					.findViewById(R.id.play_name);
+			mViewHolder.mButton = (Button) convertView
+					.findViewById(R.id.play_price);
+			convertView.setTag(mViewHolder);
+		}
+		mViewHolder.mTextView.setText(mPlayList.get(position).getName());
+		if (mPlayList.get(position).getPrice() == 0)
+			mViewHolder.mButton.setText("FREE");
+		else
+			mViewHolder.mButton
+					.setText("$"
+							+ String.format("%.2f", mPlayList.get(position)
+									.getPrice()));
+		mViewHolder.mButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			}
+		});
+		return convertView;
+	}
+
+	final static class ViewHolder {
+		TextView mTextView;
+		Button mButton;
+	}
+}
