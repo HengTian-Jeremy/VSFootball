c20e31196a45d25862de103e6bf572e7b8426692
jiayunhuang <jiayunhuang@hengtiansoft.com>
2013-07-29 20:25:07 +0800

    add VSFAdsBannerView files

---
+++ ios/VsFootball/VsFootball/Common/CustomView/VSFADBannerView.h
@@ -0,0 +1,29 @@
+//
+//  VSFADBannerView.h
+//  VsFootball
+//
+//  Created by hjy on 13-7-29.
+//  Copyright (c) 2013年 engagemobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/*!
+ @class VSFADBannerView
+ 
+ @abstract AdBanner view
+ 
+ @discussion AdBanner view
+ */
+@interface VSFADBannerView : UIView
+
+/*!
+ @method getAdBannerView
+ @abstract get VSFADBannerView instance
+ @discussion get VSFADBannerView instance
+ @param NULL
+ @result VSFADBannerView instance
+ */
++ (VSFADBannerView *)getAdBannerView;
+
+@end
